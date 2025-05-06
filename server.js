4199787a355960a7985b391771810d999e69f7db
Rijokj <rijokj199@gmail.com>
2025-05-06 11:32:35 +0530

    first commit

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const User = require("./models/userSchema")
+const express = require('express')
+const mongoose = require('mongoose')
+const User = require('./models/userSchema')
 const Cors = require('cors')
-const app =express()
+const bcrypt = require('bcrypt')
+const app = express()
 
 app.use(Cors())
 app.use(express.json())
 
-mongoose.connect(
-  'mongodb+srv://rijokj199:qwerty123@cluster0.58x8a.mongodb.net/imca?retryWrites=true&w=majority&appName=Cluster0'
-).then(()=>{
-    console.log("mogoose is connected");
-    
-})
+mongoose
+  .connect(
+    'mongodb+srv://rijokj199:qa1234@cluster0.gpqq2gm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => {
+    console.log('mogoose is connected')
+  })
 
-app.post('/',async(req,res)=>{
-    try {
-        const userData = await User.create({name:req.body.name,password:req.body.password})
-        
-        if(userData){
-            res.status(200).json({message:"Signup Completed"})
-        }else{
-            res.status(404).json({message:"Something went Wrong"})
-        }
+app.post('/', async (req, res) => {
+  try {
+    const password = req.body.password
+    // const hashedPassword = await bcrypt.hash(password, 10)
+    const userData = await User.create({
+      name: req.body.name,
+      password: password,
+    })
 
-
-    } catch (error) {
-       res.status(500)
-         .json({ message: 'Error during signUp', error: error.message })
+    if (userData) {
+      res.status(200).json({ message: 'Signup Completed' })
+    } else {
+      res.status(404).json({ message: 'Something went Wrong' })
     }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Error during signUp', error: error.message })
+  }
 })
 
-app.post('/login',async(req,res)=>{
-    try {
-        const loginUser = await User.findOne({name:req.body.name})
-        if(!loginUser){
-            return res.status(404).json({message:"User not found"})
-        }
-        if(loginUser.password !==req.body.password){
-            return res.status(401).json({message:"Credential Missmatch"})
-        }
-        res.status(200).json({message:"login successfull"})
-    } catch (error) {
-        console.log(error.message);
-        
+app.post('/login', async (req, res) => {
+  try {
+    const password = req.body.password
+
+    const loginUser = await User.findOne({ name: req.body.name })
+    if (!loginUser) {
+      return res.status(404).json({ message: 'User not found' })
     }
+
+    // const isMatch = await bcrypt.compare(password, loginUser.password)
+    
+ 
+    if (password !== loginUser.password ) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+    res.status(200).json({ message: 'Login successful' })
+  } catch (error) {
+    console.log(error.message)
+  }
 })
 
-
-app.listen(3077,()=>{
-    console.log("server is running");
-    
+app.listen(3077, () => {
+  console.log('server is running')
 })
